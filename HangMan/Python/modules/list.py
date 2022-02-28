4ca3727ca89deecd359ac81458788f7199af686a
PanzerKrzysiek <86798628+PanzerKrzysiek@users.noreply.github.com>
2022-02-28 21:49:42 +0100

    Add files via upload

---
+++ HangMan/Python/modules/list.py
@@ -0,0 +1,36 @@
+#############################
+# list.py by PanzerKrzysiek #
+#############################
+
+"""   This module stores the list of the words used in the game.   """
+
+list_of_words = [
+"Akrobacja",  "Ananas",      "Anakonda",
+"Beton",      "Biedronka",   "Bzdury",
+"Cebula",     "Cytryna",     "Czytnik",
+"Ćwiczenia",  "Ćwierć",      "Ćwikła",
+"Daktyl",     "Doktor",      "Dynamit",
+"Edukacja",   "Emocje",      "Empatia",
+"Fantazja",   "Francja",     "Frytki",
+"Gorczyca",   "Górnik",      "Groza",
+"Hejnał",     "Herbata",     "Horyzont",
+"Indeks",     "Informacja",  "Internet",
+"Jemioła",    "Jeździec",    "Jordania",
+"Kapusta",    "Komentarz",   "Korniszon",
+"Lekkość",    "Leszcz",      "Licencja",
+"Łazanki",    "Łazienka",    "Łączność",
+"Makaron",    "Malina",      "Mauretania",
+"Nagłówek",   "Napiwek",     "Nosorożec",
+"Obierki",    "Ogórek",      "Optymizm",
+"Ósemka",     "Ósemkowy",    "Ówczesny",
+"Pagórek",    "Pełnia",      "Piernik",
+"Rakietki",   "Remiza",      "Rumunia",
+"Sierp",      "Sążeń",       "Sadzawka",
+"Ściana",     "Śmierć",      "Śmietana",
+"Terkot",     "Trzcina",     "Twierdza",
+"Ukwiał",     "Ułamek",      "Urugwaj",
+"Wakacje",    "Wielbłąd",    "Wiosło",      
+"Zapylacz",   "Zarząd",      "Złotówka",
+"Źdźbło",     "Źrenica",     "Źródło",
+"Żarówka",    "Żądło",       "Żołądek"
+]
